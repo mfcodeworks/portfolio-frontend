8eed892b6d29cf54a578509bb6f53e412d4ede03
Arran Fletcher <arran.fletcher@linqcash.com>
2020-05-25 03:06:23 +0800

    Updated base href check and routing strategy for ipfs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
 ];
 
 const options: ExtraOptions = {
-    useHash: false,
+    useHash: ['ipfs', 'ipns'].includes(window.location.pathname.split('/')[1].toLowerCase()),
     scrollPositionRestoration: 'enabled',
     anchorScrolling: 'enabled'
 };
